6c598c79ac2a85333b8e8bf86eb17d833ccff76c
z003bwq <maria.stommes@target.com>
2019-01-26 13:08:39 -0600

    adds the server to the front end code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 

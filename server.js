58b4210c540e871a49fdb09fa3f712c0136e0e1f
z003bwq <maria.stommes@target.com>
2019-01-26 14:51:36 -0600

    removing lock

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+const dataStore = require('nedb');
+const db = new dataStore({ filename: 'db/catData.json', autoload: true });
+
+var cat = { houseNumber: 1, weight: 'weight', houseTemp: 'temp', today: new Date()};
+
+db.insert(cat, function (err, newCat) {   
+    if(err){
+        console.log("There was an error entering the cat into the db");
+    }else{
+        console.log("entering the cat" + newCat + " to the db worked just fine.")
+    }
+});
+
+
+
 
 app.get('/', (req, res) => {
   res.send({ express: 'Hello From Express' });

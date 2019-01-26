b0557ef5467bc8e39ca3309edd35bbfc3336a9a8
Abbie Tuckner <abbietuckner@gmail.com>
2019-01-26 11:56:46 -0600

    Maybe this will work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';

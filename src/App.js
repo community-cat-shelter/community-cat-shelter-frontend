174c7a105df722ed7a61206634fff383bd017372
Melody8988 <melody.massard8988@gmail.com>
2019-01-26 18:44:22 -0600

    image

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import {
   Navbar,
   Row,
   Col,
-  Image
+  Image,
+  Panel,
+  Label
+
+
 } from 'react-bootstrap';
 import axios from 'axios';
 
 import CurrentDataPanel from './components/CurrentDataPanel';
+import styled from 'styled-components';
+
+
+const catImage = styled(Image)`
+  float: right;
+`;
 
 class App extends Component {
   componentDidMount() {
@@ -19,23 +29,30 @@ class App extends Component {
   render() {
     return (
       <div>
+
         <Navbar>
           <Navbar.Header>
-            <Navbar.Brand>
+            <Navbar.Brand >
               <a href="#home">React-Bootstrap</a>
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
         <Row>
           <Col md={4}>
-         
-          <Image src = {require("./cat_flat.jpg")} thumbnail/>
-          
+
+            <Panel style={{ float: 'right' }}>
+            <center>
+            <h1><small>Backyard Shelter</small></h1>
+            </center>
+            <Panel.Body>
+              <Image  src={require("./cat_flat.jpg")} thumbnail />
+              </Panel.Body>
+            </Panel>
           </Col>
           <Col md={3}>
           </Col>
           <Col md={4}>
-            <CurrentDataPanel/>
+            <CurrentDataPanel />
           </Col>
           <Col md={1}></Col>
         </Row>

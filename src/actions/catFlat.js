37dc0c1d33e1f524635bf5cb65ffefe15c4a344d
Kristina <gelicia@gelicia.com>
2019-01-27 08:01:56 -0600

    Replace nedb with sqlite3 (#15)
    * Fix bug with server
    
    * Replace nedb with sqlite3

--- src/actions/catFlat.js
+++ src/actions/catFlat.js
@@ -2,5 +2,14 @@ import axios from 'axios';
 
 export const getCatFlat = (limit) => {
   const queryParams = limit ? `?limit=${limit}` : ''
-  return axios.get(`http://localhost:5000/catData${queryParams}`);
+  return axios.get(`http://localhost:5000/catData${queryParams}`).then(resp => {
+    let formattedResp = []
+    resp.data.forEach(element => {
+      var utcSeconds = element.date;
+      var d = new Date(0);
+      d.setUTCSeconds(utcSeconds);
+      formattedResp.push({date: d, weight: element.weight, shelterTemp: element.shelterTemp, ambientTemp: element.ambientTemp});
+    });
+    return formattedResp;
+  });
 };
